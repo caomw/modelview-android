b923c4289f94edd5c14f490b1d4ebcbcf4a8b66b
jeffrey.blattman <jeffrey.blattman@3e32055e-e66f-8873-0999-6781ff2dd7d0>
2010-12-09 02:11:53 +0000

    minor perf and other tweaks

--- src/org/jtb/modelview/Mesh.java
+++ src/org/jtb/modelview/Mesh.java
@@ -15,7 +15,7 @@ class Mesh {
 	private int indexCount = -1;
 	private FloatBuffer colorBuffer = null;
 	private FloatBuffer normalsBuffer;
-	
+
 	// Rotate params.
 	float rx = 0f;
 	float ry = 0f;
@@ -24,17 +24,17 @@ class Mesh {
 	float dxSpeed = 0f;
 	float dySpeed = 0f;
 	float scale = 1f;
-	
+
 	Vertex max, min, mid;
 	float radius;
-	
+
 	void draw(GL10 gl) {
 		gl.glFrontFace(GL10.GL_CCW);
 
 		gl.glEnable(GL10.GL_CULL_FACE);
 
-		//gl.glEnable(GL10.GL_BLEND);
-		//gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_DST_ALPHA);
+		// gl.glEnable(GL10.GL_BLEND);
+		// gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_DST_ALPHA);
 
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, verticesBuffer);
@@ -50,9 +50,9 @@ class Mesh {
 			gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
 		}
 
-		//gl.glCullFace(GL10.GL_FRONT);
-		//gl.glDrawElements(GL10.GL_TRIANGLES, numOfIndices,
-		//		GL10.GL_UNSIGNED_SHORT, indicesBuffer);
+		// gl.glCullFace(GL10.GL_FRONT);
+		// gl.glDrawElements(GL10.GL_TRIANGLES, numOfIndices,
+		// GL10.GL_UNSIGNED_SHORT, indicesBuffer);
 		gl.glCullFace(GL10.GL_BACK);
 		gl.glDrawElements(GL10.GL_TRIANGLES, indexCount,
 				GL10.GL_UNSIGNED_SHORT, indicesBuffer);
@@ -61,7 +61,7 @@ class Mesh {
 		gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
 		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
 		gl.glDisable(GL10.GL_CULL_FACE);
-		//gl.glDisable(GL10.GL_BLEND);
+		// gl.glDisable(GL10.GL_BLEND);
 	}
 
 	protected void setTriangles(List<Triangle> tris) {
@@ -72,56 +72,74 @@ class Mesh {
 		float[] normals = Triangle.toNormals(tris);
 		setNormals(normals);
 		normals = null;
-		
+
 		int[] indices = Triangle.toIndices(tris);
 		setIndices(indices);
 		indices = null;
-		
+
 		float[] colors = Triangle.toColors(tris);
 		setColors(colors);
 		colors = null;
-		
+
 		mid = min.middle(max);
 		radius = Triangle.boundingRadius(tris, mid);
 	}
 
 	private void setVertices(float[] vertices) {
-		verticesBuffer = FloatBuffer.wrap(vertices);
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * Float.SIZE
+				/ 8);
+		vbb.order(ByteOrder.nativeOrder());
+		verticesBuffer = vbb.asFloatBuffer();
+		verticesBuffer.put(vertices);
 		verticesBuffer.position(0);
 	}
 
 	private void setNormals(float[] normals) {
-		normalsBuffer = FloatBuffer.wrap(normals);
+		ByteBuffer vbb = ByteBuffer.allocateDirect(normals.length * Float.SIZE
+				/ 8);
+		vbb.order(ByteOrder.nativeOrder());
+		normalsBuffer = vbb.asFloatBuffer();
+		normalsBuffer.put(normals);
 		normalsBuffer.position(0);
 	}
 
 	private void setIndices(int[] indices) {
+		ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * Short.SIZE
+				/ 8);
+		ibb.order(ByteOrder.nativeOrder());
+		indicesBuffer = ibb.asShortBuffer();
+
 		short[] shorts = new short[indices.length];
 		for (int i = 0; i < shorts.length; i++) {
 			shorts[i] = (short) indices[i];
 		}
-		
-		indicesBuffer = ShortBuffer.wrap(shorts);
+		indicesBuffer.put(shorts);
+		shorts = null;
 		indicesBuffer.position(0);
-		indexCount = shorts.length;
+		indexCount = indices.length;
 	}
 
 	private void setColors(float[] colors) {
-		colorBuffer = FloatBuffer.wrap(colors);
+		// float has 4 bytes.
+		ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+		cbb.order(ByteOrder.nativeOrder());
+		colorBuffer = cbb.asFloatBuffer();
+		colorBuffer.put(colors);
 		colorBuffer.position(0);
 	}
 
 	void dampenSpeed(long deltaMillis) {
 		if (dxSpeed != 0.0f) {
 			dxSpeed *= (1.0f - 0.001f * deltaMillis);
-			if (Math.abs(dxSpeed) < 0.001f) dxSpeed = 0.0f;
+			if (Math.abs(dxSpeed) < 0.001f)
+				dxSpeed = 0.0f;
 		}
-		
+
 		if (dySpeed != 0.0f) {
 			dySpeed *= (1.0f - 0.001f * deltaMillis);
-			if (Math.abs(dySpeed) < 0.001f) dySpeed = 0.0f;
+			if (Math.abs(dySpeed) < 0.001f)
+				dySpeed = 0.0f;
 		}
 	}
-	
-	
+
 }
